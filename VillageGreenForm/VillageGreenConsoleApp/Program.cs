3b074978c41dfba40a7bc05e86c2f16a04787bd3
Thibaud <thibaud.delabr@gmail.com>
2016-03-01 10:33:05 +0100

    fin de la méthode CheckMail

--- VillageGreenForm/VillageGreenConsoleApp/Program.cs
+++ VillageGreenForm/VillageGreenConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace VillageGreenConsoleApp
@@ -10,6 +11,50 @@ namespace VillageGreenConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Entrez votre adresse email");
+            string adresseEmail = Console.ReadLine();
+
+            Console.WriteLine(CheckMail(adresseEmail)); 
+        }
+
+
+/// <summary>
+/// Methode pour valider une adresse email
+/// </summary>
+/// <param name="eMail"></param>
+/// <returns></returns>
+        static string CheckMail(string eMail)
+        {
+            string result = "";
+            if (Regex.IsMatch(eMail, @"^(\w){2,}\@(\w){2,}\.[a-z]{2,}$"))
+            {
+                result = "";
+            }
+            else if (Regex.IsMatch(eMail, @"^(\w){1}\@(\w){2,}\.[a-z]{2,}$"))
+            {
+                result = "L'adresse doit comporter au moins deux caractères devant @";
+            }
+            else if (Regex.IsMatch(eMail, @"^(\w){2,}\@(\w){1}\.[a-z]{2,}$"))
+            {
+                result = "L'adresse doit comporter au moins deux caractères après @ ";
+            }
+            else if (Regex.IsMatch(eMail, @"^(\w){1}\@(\w){1}\.[a-z]{2,}$"))
+            {
+                result = "L'adresse doit comporter au moins deux caractères devant et après @ ";
+            }
+            else if (Regex.IsMatch(eMail, @"^(\w){2,}\@(\w){2,}$"))
+            {
+                result = "L'adresse doit comporter au moins un point dans la partie qui suit @";
+            }
+            else if (Regex.IsMatch(eMail, @"^(\w){2,}\@(\w){2,}\.[a-z]{1}$"))
+            {
+                result = "L'adresse doit comporter après le point au moins deux caractères";
+            }
+            else
+            {
+                result = "Adresse email invalide";
+            }
+            return result;
         }
     }
 }
