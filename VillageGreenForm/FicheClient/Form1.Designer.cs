e2fac4e8e91643a1913c5b1d92513b9ba8a79c64
Thibaud <thibaud.delabr@gmail.com>
2016-03-02 12:11:57 +0100

    test

--- VillageGreenForm/FicheClient/Form1.Designer.cs
+++ VillageGreenForm/FicheClient/Form1.Designer.cs
@@ -35,7 +35,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -103,21 +102,11 @@
             this.button1.Text = "Catalogue";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(420, 382);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(131, 49);
-            this.button5.TabIndex = 47;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1161, 679);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
@@ -139,7 +128,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button5;
     }
 }
 
